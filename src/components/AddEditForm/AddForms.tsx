21bba12e3044a36d863144c78f9ed059440f2d79
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2025-02-18 09:42:56 +0100

    Try to fix the bug in adding the Income in deployment.

--- src/components/AddEditForm/AddForms.tsx
+++ src/components/AddEditForm/AddForms.tsx
@@ -115,6 +115,8 @@ const AddForms: React.FC = () => {
                 throw error;
             }
 
+            console.log("Selected category ID:", selectCategory);
+
             // Empty data from the useStates
             setExpenseDesc("");
             setExpenseAmount(undefined);
